d76d632a3880b68373716bdb688b3e89a8669745
ryo-murai <murai.ry@gmail.com>
2019-03-18 18:40:56 +0900

    fix csv line breaks in values and download file title etc

--- ext/js/moneyforward_csv_download.js
+++ ext/js/moneyforward_csv_download.js
@@ -1,17 +1,33 @@
 (function murtools() {
+    const mfSite = "https://moneyforward.com/cf";
+    if(mfSite != location.href) {
+        if(confirm(`This Bookmarklet works only on ${mfSite}\n Do you want to move that site ?`)) {
+            window.open(mfSite);
+        } else {
+            return;
+        }
+    }
+
+    const csvTitleElems = $('#in_out .fc-header-title > h2');
+    const csvTitle = (csvTitleElems.length == 1) ? 
+            csvTitleElems.text().replace(/\//g, "").replace(/ /g, "") : "";
+
     const rows = $('#cf-detail-table tr').toArray();
-    const csvdata = rows.map(
-        row => $('td', row).toArray()
-                .map(td => `"${td.innerText}"`)
+    const csvdata = rows
+        .filter(row => 
+            $('td', row).first().find('i.icon-check').length > 0)
+        .map(row => $('td', row).toArray()
+                .map(td => `"${td.innerText}"`.replace(/\n/g, ' '))
                 .join(',')
     ).join('\n');
 
     const blob = new Blob([ csvdata ], { "type" : "text/csv" });
     const url = window.URL || window.webkitURL;
     $("<a>", {
-        class: 'btn',
-        id: 'my_dl_csv',
+        class: 'btn btn-info',
+        id: `csv-${csvTitle}`,
+        download: `${csvTitle}.csv`,
         href: url.createObjectURL(blob),
-        text: 'CSV Download'
+        text: `DL ${csvTitle}`
     }).appendTo('#js-dl-area');
 })();
